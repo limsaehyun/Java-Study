2867f35befb1c8a3ca3399bdade6893251823596
saehyun <sh00710000@gmail.com>
2021-09-18 19:45:36 +0900

    Feat :: MyProgram

--- Strategy/src/MyProgram.java
+++ Strategy/src/MyProgram.java
@@ -1,4 +1,30 @@
 public class MyProgram {
 
+    private SearchButton searchButton = new SearchButton(this);
 
+    public void setModeAll () {
+        searchButton.setSearchStrategy(new SearchStrategyAll());
+    }
+
+    public void setModeImage () {
+        searchButton.setSearchStrategy(new SearchStrategyImage());
+    }
+
+    public void setModeNews () {
+        searchButton.setSearchStrategy(new SearchStrategyNews());
+    }
+
+    public void setModeMap () {
+        searchButton.setSearchStrategy(new SearchStrategyMap());
+    }
+
+    public void testProgram () {
+        searchButton.onClick();
+        setModeImage();
+        searchButton.onClick();
+        setModeNews();
+        searchButton.onClick();
+        setModeMap();
+        searchButton.onClick();
+    }
 }
