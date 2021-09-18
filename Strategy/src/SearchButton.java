a001e3e7154057ec9f31007bef76cba8d57b11dc
saehyun <sh00710000@gmail.com>
2021-09-18 19:45:46 +0900

    Feat :: SearchButton

--- Strategy/src/SearchButton.java
+++ Strategy/src/SearchButton.java
@@ -1,2 +1,18 @@
 public class SearchButton {
+
+    private MyProgram myProgram;
+
+    public SearchButton (MyProgram _myProgram) {
+        myProgram = _myProgram;
+    }
+
+    private SearchStrategy searchStrategy = new SearchStrategyAll();
+
+    public void setSearchStrategy (SearchStrategy _searchStrategy) {
+        searchStrategy = _searchStrategy;
+    }
+
+    public void onClick() {
+        searchStrategy.search();
+    }
 }
