82abfe0bc966e946e9870ee8e5b234ba70895954
saehyun <sh00710000@gmail.com>
2021-09-19 22:42:41 +0900

    Refactor :: Method

---
+++ 자바 기능 연습/Calculator/src/CalculatorDemo2.java
@@ -0,0 +1,12 @@
+public class CalculatorDemo2 {
+
+    public static void sum(int left, int right) {
+        System.out.println(left + right);
+    }
+
+    public static void main(String[] args) {
+        sum(10, 20);
+        sum(20, 40);
+    }
+
+}
