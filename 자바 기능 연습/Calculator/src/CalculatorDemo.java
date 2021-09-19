680a634f4f9612cdef67f6848cec4a92b9f64fb4
saehyun <sh00710000@gmail.com>
2021-09-19 22:42:34 +0900

    Feat :: CalculatorDemo

---
+++ 자바 기능 연습/Calculator/src/CalculatorDemo.java
@@ -0,0 +1,9 @@
+public class CalculatorDemo {
+
+    public static void main(String[] args) {
+        // 아래의 로직이 1000줄 짜리의 복잡한 로직이라고 가정
+        System.out.println(10 + 20);
+        System.out.println(20 + 40);
+    }
+
+}
